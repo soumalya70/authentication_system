445d66f16f88de90a3904d1004f5373c70e767a9
soumalya70 <soumalyasaha200@gmail.com>
2023-10-13 22:45:36 +0530

    first commit

---
+++ prj/info.py
@@ -0,0 +1,8 @@
+#Email - SMTP From Gmail
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER="soumalyasaha200@gmail.com"
+EMAIL_HOST_PASSWORD="mjtceibepblkdisg"
+EMAIL_USE_TLS=True
